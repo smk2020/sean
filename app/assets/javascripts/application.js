79cece4445345e464444137e42a1d8f7c6d26fcd
Reggie Tucker <reggiest09@gmail.com>
2015-05-16 14:09:52 -0400

    fitst

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min.js
+//= require jquery.scrollTo-1.4.3.1-min.js
+//= require jquery.localscroll-1.2.7-min.js
+//= require jquery.easing.min.js
+//= require jquery.parallax-1.1.3.js
+//= require google-map.js
+//= require twitter-fetcher.js
+//= require jpreloader.min.js
+//= require imagesloaded.pkgd.js
+//= require isotope.pkgd.min.js
+//= require wow.js
+//= require jquery.flexslider-min.js
+//= require jquery.magnific-popup.min.js
+//= require supersized.3.2.7.min.js
+//= require supersized.shutter.min.js
+//= require loop.js
